b38f4b8b31f8730852f657ad36f6b44bf27cb7d3
Rameesha Khan <rameeshakhan75@gmail.com>
2022-12-04 19:24:21 +0500

    Swap methods

---
+++ Swap.py
@@ -0,0 +1,21 @@
+x= 10;
+y= 9;
+print("before swaping x and y are ",x,y)
+
+# def swap(a,b):
+#     temp=a
+#     a=b
+#     b=temp
+#     print(a,b)
+
+def swap(a,b):
+    a=a+b
+    b=a-b
+    a=a-b
+    return a,b
+    
+print("after swaping x and y are ",swap(x,y))
+
+# output
+# before swaping x and y are  10 9
+# after swaping x and y are  (9, 10)
